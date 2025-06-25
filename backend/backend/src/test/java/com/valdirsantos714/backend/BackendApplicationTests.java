1eb8cba372faacbfca5efaf1db2d5ac6cf042467
Valdir Santos <valdirsantost40@gmail.com>
2025-06-25 18:46:12 -0300

    iniciado projeto e configurado jest no frontend

---
+++ backend/backend/src/test/java/com/valdirsantos714/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdirsantos714.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
