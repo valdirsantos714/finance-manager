cd6cd39d181f951f5f59101ce07d29bf6b235fd7
Valdir Santos <valdirsantost40@gmail.com>
2025-06-30 18:47:01 -0300

    implementado métodos que consomem os métodos do service

--- frontend/src/app/components/despesa-dashboard/despesa-dashboard.component.ts
+++ frontend/src/app/components/despesa-dashboard/despesa-dashboard.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DespesasResponse } from '../../models/DespesasResponse';
+import { Subject, takeUntil } from 'rxjs';
+import { DespesaService } from '../../services/despesa-service/despesa.service';
+import { DespesasRequest } from '../../models/DespesasRequest';
 
 @Component({
   selector: 'app-despesa-dashboard',
   templateUrl: './despesa-dashboard.component.html',
   styleUrl: './despesa-dashboard.component.css'
 })
-export class DespesaDashboardComponent {
+export class DespesaDashboardComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  public despesas!: DespesasResponse[];
+
+  constructor(private despesaService: DespesaService ) { }
+
+  ngOnInit(): void {
+    this.getDespesas();
+  }
+
+  getDespesas(): void {
+    this.despesaService.getAllDespesas()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (despesas: DespesasResponse[]) => {
+          this.despesas = despesas;
+        },
+        error: (error) => {
+          console.error('Error fetching despesas:', error);
+        }
+      });
+  }
+
+  updateDespesa(idDespesa: number, updatedDespesa: DespesasRequest): void {
+    this.despesaService.updateDespesa(idDespesa, updatedDespesa)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (updatedDespesaResponse: DespesasResponse) => {
+          console.log('Despesa updated successfully:', updatedDespesaResponse);
+          this.getDespesas(); // Refresh the list after update
+        },
+        error: (error) => {
+          console.error('Error updating despesa:', error);
+        }
+      });
+  }
+
+  deleteDespesa(idDespesa: number): void {
+    this.despesaService.deleteDespesa(idDespesa)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          console.log('Despesa deleted successfully');
+          this.getDespesas(); // Refresh the list after deletion
+        },
+        error: (error) => {
+          console.error('Error deleting despesa:', error);
+        }
+      });
+    }
+
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
 }
--- frontend/src/app/components/renda-dashboard/renda-dashboard.component.ts
+++ frontend/src/app/components/renda-dashboard/renda-dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { RendaService } from '../../services/renda.service';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { RendasResponse } from '../../models/RendasResponse';
+import { RendaService } from '../../services/renda-service/renda.service';
 
 @Component({
   selector: 'app-renda-dashboard',
@@ -30,6 +30,34 @@ export class RendaDashboardComponent implements OnInit, OnDestroy {
         console.error('Error fetching rendas:', error);
       }
     });
+  }
+
+  updateRenda(idRenda: number): void {
+    this.rendaService.updateRenda(idRenda)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (updatedRenda: RendasResponse) => {
+          console.log('Renda updated successfully:', updatedRenda);
+          this.getRendas(); 
+        },
+        error: (error) => {
+          console.error('Error updating renda:', error);
+        }
+      });
+  }
+
+  deleteRenda(idRenda: number): void {
+    this.rendaService.deleteRenda(idRenda)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          console.log('Renda deleted successfully');
+          this.getRendas(); 
+        },
+        error: (error) => {
+          console.error('Error deleting renda:', error);
+        }
+      });
   }
 
   
