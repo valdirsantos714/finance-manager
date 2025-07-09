4d9c78a310d49eed8b8509cfb02751210b6f1b5c
Valdir Santos <valdirsantost40@gmail.com>
2025-07-09 18:44:56 -0300

    corrigido bug do AuthGuard

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,28 +1,32 @@
 import { CanActivateFn, Router } from '@angular/router';
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
   const publicRoutes = ['/login', '/register'];
 
   if (publicRoutes.includes(state.url)) {
     return true;
   }
 
-  const cookies = document.cookie.split(';');
-  let token = null;
-  for (let i = 0; i < cookies.length; i++) {
-    let cookie = cookies[i].trim();
-    if (cookie.startsWith('jwt=')) {
-      token = cookie.substring(4);
-      break;
+  if (isPlatformBrowser(platformId)) {
+    const cookies = document.cookie.split(';');
+    let token = null;
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i].trim();
+      if (cookie.startsWith('jwt=')) {
+        token = cookie.substring(4);
+        break;
+      }
+    }
+
+    if (token) {
+      return true;
     }
   }
 
-  if (token) {
-    return true;
-  } else {
-    router.navigate(['/login']);
-    return false;
-  }
+  router.navigate(['/login']);
+  return false;
 };
